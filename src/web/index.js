1364d2b2eb21bbf65644dd0f71d461b631701a5e
alaustrup <83102227+ALaustrup@users.noreply.github.com>
2026-05-03 00:36:32 -0700

    Deprecate web-app interface; prepare for landing page

--- src/web/index.js
+++
@@ -1,9 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from '../../App';
-import { name as appName } from '../../app.json';
-
-AppRegistry.registerComponent('main', () => App);
-AppRegistry.runApplication('main', {
-  initialProps: {},
-  rootTag: document.getElementById('root'),
-});
